8624945e7977a2a0b16fed802d9b262b961cdf43
Pascal Giammelluca <pascal@pgmp.dev>
2019-09-15 18:49:09 +0200

    Add basic single-target selection

---
+++ Assets/Scripts/Game/GameCursorSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCursorSelection : MonoBehaviour
+{
+    public GameObject selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rh;
+            if (Physics.Raycast(ray, out rh, Camera.main.farClipPlane))
+            {
+                selected = rh.collider.gameObject;
+            }
+        }
+    }
+}
